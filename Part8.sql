f3e1f1f0a8ec75bc6e7270f6e5681207b4922d87
FauquembertFred <fredfauq@sfr.fr>
2018-09-18 08:49:36 +0200

    Exercices SQL

--- Part8.sql
+++ Part8.sql
@@ -1,6 +1,6 @@
 -- SQL - Partie 8 : Les jointures
 -- Toutes les tables à utiliser sont dans le fichier bdd.sql
-
+USE `webdevelopment`;
 -- Exercice 1
 -- Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework l'afficher aussi.
 -- sélectionner dans la table language et frameworks les name
@@ -12,23 +12,23 @@ ON `languages`.`id`=`frameworks`.`languagesId`;
 -- Exercice 2
 -- Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework ne pas l'afficher.
 SELECT * FROM `frameworks`
-INNER JOIN `languages` -- critères de sélection
+INNER JOIN `languages` -- critère de sélection
 ON `frameworks`.`languagesId`=`languages`.`id`;
 
 -- Exercice 3
--- Afficher le nombre de framework qu'a un language.
+-- Afficher le nombre de framework par language.
 SELECT `languages`.`name`, count(`frameworks`.`id`) 
--- la fonction count permet de compter le nombre de ligne(le nombre d'enregistrement) dans le tableau
+-- la fonction count permet de compter le nombre de ligne (le nombre de champs) dans le tableau
 FROM `languages`
 INNER JOIN  `frameworks`
 ON `frameworks`.`languagesId`=`languages`.`id`
-GROUP BY `languages`.`name`; -- critéres de sélection
+GROUP BY `languages`.`name`; -- critére de sélection
 
 -- Exercice 4
 -- Afficher les langages ayant plus de 3 frameworks.
-SELECT `languages`.`name`, count(`frameworks`.`id`) AS Nb_framework
+SELECT `languages`.`name`, count(`frameworks`.`id`) AS `nbFramework`
 FROM `languages`
 INNER JOIN `frameworks`
 ON `frameworks`.`languagesId`=`languages`.`id`
 GROUP BY `languages`.`name`
-HAVING Nb_framework>'3';
+HAVING `nbFramework`>'3';
