c9476b889e4577911191bd3dc6ffd80369bcd7f7
FauquembertFred <fredfauq@sfr.fr>
2018-09-18 08:52:01 +0200

    Exercice SQL

--- Part8.sql
+++ Part8.sql
@@ -1,6 +1,6 @@
 -- SQL - Partie 8 : Les jointures
 -- Toutes les tables à utiliser sont dans le fichier bdd.sql
-USE `webdevelopment`;
+USE `development`;
 -- Exercice 1
 -- Afficher tous les frameworks associés à leurs langages. Si un langage n'a pas de framework l'afficher aussi.
 -- sélectionner dans la table language et frameworks les name
